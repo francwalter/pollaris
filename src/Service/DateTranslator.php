e03def1532e70394e7ba0d2a86539f80caaee0b5
Marien Fressinaud <dev@marienfressinaud.fr>
2024-08-22 22:16:15 +0200

    dev: Allow to translate dates

---
+++ src/Service/DateTranslator.php
@@ -0,0 +1,45 @@
+<?php
+
+// This file is part of Pollaris.
+// Copyright 2022-2024 Probesys (Bileto)
+// Copyright 2024 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class DateTranslator
+{
+    public function __construct(
+        private RequestStack $requestStack,
+    ) {
+    }
+
+    public function format(\DateTimeInterface $date, string $format = 'dd MMM Y, HH:mm'): string
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if ($request === null) {
+            throw new \Exception('Cannot translate the date (request is null)');
+        }
+
+        $currentLocale = $request->getLocale();
+
+        $formatter = new \IntlDateFormatter(
+            $currentLocale,
+            \IntlDateFormatter::FULL,
+            \IntlDateFormatter::FULL,
+            null,
+            null,
+            $format
+        );
+
+        $translatedDate = $formatter->format($date);
+        if ($translatedDate === false) {
+            throw new \Exception("Cannot translate the date (format: {$format})");
+        }
+
+        return $translatedDate;
+    }
+}
---
+++ src/Twig/DateFormatterExtension.php
@@ -0,0 +1,85 @@
+<?php
+
+// This file is part of Pollaris.
+// Copyright 2022-2024 Probesys (Bileto)
+// Copyright 2024 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Twig;
+
+use App\Service;
+use App\Utils;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class DateFormatterExtension extends AbstractExtension
+{
+    public function __construct(
+        private Service\DateTranslator $dateTranslator,
+    ) {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('dateTrans', [$this, 'dateTrans']),
+            new TwigFilter('dateIso', [$this, 'dateIso']),
+            new TwigFilter('dateFull', [$this, 'dateFull']),
+            new TwigFilter('dateShort', [$this, 'dateShort']),
+        ];
+    }
+
+    public function dateTrans(\DateTimeInterface $date, string $format = 'dd MMM yyyy, HH:mm'): string
+    {
+        return $this->dateTranslator->format($date, $format);
+    }
+
+    public function dateIso(\DateTimeInterface $date): string
+    {
+        return $date->format(\DateTimeInterface::ATOM);
+    }
+
+    public function dateFull(\DateTimeInterface $date, bool $fullMonth = false, bool $cleverYear = false): string
+    {
+        $today = Utils\Time::relative('today');
+        $currentYear = $today->format('Y');
+        $dateYear = $date->format('Y');
+
+        $format = 'dd';
+
+        if ($fullMonth) {
+            $format .= ' MMMM';
+        } else {
+            $format .= ' MMM';
+        }
+
+        if (!$cleverYear || $currentYear !== $dateYear) {
+            $format .= ' yyyy';
+        }
+
+        $format .= ', HH:mm';
+
+        return $this->dateTrans($date, $format);
+    }
+
+    public function dateShort(\DateTimeInterface $date, bool $fullMonth = false, bool $cleverYear = false): string
+    {
+        $today = Utils\Time::relative('today');
+        $currentYear = $today->format('Y');
+        $dateYear = $date->format('Y');
+
+        $format = 'dd';
+
+        if ($fullMonth) {
+            $format .= ' MMMM';
+        } else {
+            $format .= ' MMM';
+        }
+
+        if (!$cleverYear || $currentYear !== $dateYear) {
+            $format .= ' yyyy';
+        }
+
+        return $this->dateTrans($date, $format);
+    }
+}
