e05f63f4cbce90186fc85e0eac93bbcb55e8122f
Marien Fressinaud <dev@marienfressinaud.fr>
2024-10-29 23:08:48 +0100

    tec: Update the Composer dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-// Workaround for https://github.com/symfony/symfony/issues/53812
-set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
