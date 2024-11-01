a96f6a00d2f2e7fdaab04557c57936e84d5da434
Marien Fressinaud <dev@marienfressinaud.fr>
2024-11-01 19:34:04 +0100

    dev: Provide a modal system

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,6 +2,10 @@ import * as Turbo from '@hotwired/turbo';
 import { Application } from '@hotwired/stimulus';
 
 import CollectionController from './controllers/collection_controller.js';
+import ModalController from './controllers/modal_controller.js';
+import ModalOpenerController from './controllers/modal_opener_controller.js';
 
 const application = Application.start();
 application.register('collection', CollectionController);
+application.register('modal', ModalController);
+application.register('modal-opener', ModalOpenerController);
---
+++ assets/js/controllers/modal_opener_controller.js
@@ -0,0 +1,44 @@
+// This file is part of Pollaris.
+// Copyright 2020-2022 Marien Fressinaud (Flus)
+// Copyright 2022-2024 Probesys (Bileto)
+// Copyright 2024 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static get values () {
+        return {
+            selector: String,
+        };
+    }
+
+    connect () {
+        this.element.setAttribute('aria-haspopup', 'dialog');
+        this.element.setAttribute('aria-controls', 'modal');
+    }
+
+    fetch (event) {
+        event.preventDefault();
+        this.dispatchOpenModalEvent('fetch');
+    }
+
+    copy (event) {
+        event.preventDefault();
+        this.dispatchOpenModalEvent('copy');
+    }
+
+    dispatchOpenModalEvent (mode) {
+        const modal = document.getElementById('modal');
+
+        const openModalEvent = new CustomEvent('open-modal', {
+            detail: {
+                target: this.element,
+                mode: mode,
+                selector: this.selectorValue,
+            },
+        });
+
+        modal.dispatchEvent(openModalEvent);
+    }
+};
