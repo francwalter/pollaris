cdc23aa7a6d9c2934805ab92d24b6b3a463abd5d
Marien Fressinaud <dev@marienfressinaud.fr>
2025-03-06 21:34:31 +0100

    imp: Always display poll details and summary

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,7 +9,6 @@ import ModalController from './controllers/modal_controller.js';
 import ModalOpenerController from './controllers/modal_opener_controller.js';
 import NotificationController from './controllers/notification_controller.js';
 import SlotsApplierController from './controllers/slots_applier_controller.js';
-import ViewController from './controllers/view_controller.js';
 
 const application = Application.start();
 application.register('calendar', CalendarController);
@@ -20,4 +19,3 @@ application.register('modal', ModalController);
 application.register('modal-opener', ModalOpenerController);
 application.register('notification', NotificationController);
 application.register('slots-applier', SlotsApplierController);
-application.register('view', ViewController);
