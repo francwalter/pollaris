19ef7f3f2a2e4c424d162414cf0a792a7a5a8c74
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-06-29 15:53:36 +0200

    dev: Provide a ILIKE DQL function

---
+++ src/Doctrine/Functions/ILike.php
@@ -0,0 +1,36 @@
+<?php
+
+// This file is part of Pollaris.
+// Copyright 2024-2025 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Doctrine\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+class ILike extends FunctionNode
+{
+    private Node $field;
+    private Node $query;
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        $fieldExpression = $this->field->dispatch($sqlWalker);
+        $queryExpression = $this->query->dispatch($sqlWalker);
+        return "({$fieldExpression} ILIKE {$queryExpression})";
+    }
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $this->field = $parser->StringPrimary();
+        $parser->match(TokenType::T_COMMA);
+        $this->query = $parser->StringPrimary();
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+}
