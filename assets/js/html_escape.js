b0bd05a2c238ab6fb327db65b6dd5cbad27345b7
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-09-28 14:21:00 +0200

    chore(js): Add utility function to escape HTML

---
+++ assets/js/html_escape.js
@@ -0,0 +1,12 @@
+// This file is part of Pollaris.
+// Copyright 2024-2025 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+export default function (text) {
+    text = text.replaceAll('&', '&amp;');
+    text = text.replaceAll('<', '&lt;')
+    text = text.replaceAll('>', '&gt;')
+    text = text.replaceAll('"', '&quot;')
+    text = text.replaceAll("'", '&#039;');
+    return text;
+}
